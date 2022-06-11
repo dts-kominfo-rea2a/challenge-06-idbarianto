1a0e8a81509c3000e3d03378545df4e11496b05a
Sugeng Arif Barianto <sugengarifbarianto@Barianto-MacBook-Pro.local>
2022-06-11 22:45:03 +0700

    feat: kuis

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,36 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+// const bacaData = null;
+
+const bacaData = (fnCallback) => {
+
+  fs.readFile(file1, 'utf8', (err, data) => {
+      let data_file1  = JSON.parse(data);
+      let data1       = data_file1.message
+
+      fs.readFile(file2, 'utf8', (err, data) => {
+          let data_file2  = JSON.parse(data);
+          let data2       = data_file2[0].message
+
+          fs.readFile(file3, 'utf8', (err, data) => {
+
+              let data_file3  = JSON.parse(data);
+              let data3       = data_file3[0].data.message
+
+              let post_data   = [ data1.split(" ")[1], data2.split(" ")[1], data3.split(" ")[1] ];
+
+              fnCallback(err, post_data);
+
+            });
+
+        });
+
+    });
+
+};
+
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
